19861640630ef2d1b5bc2fb7c9117817456460f2
PaulBallestra <paulballestra@gmail.com>
2026-04-18 19:55:57 +0200

    :tada: Project Init

---
+++ src/main/java/fr/seeeek/carpenterprotocol/CarpenterProtocol.java
@@ -0,0 +1,22 @@
+package fr.seeeek.carpenterprotocol;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class CarpenterProtocol extends JavaPlugin {
+
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+
+    public CarpenterProtocol(@Nonnull JavaPluginInit init) {
+        super(init);
+        LOGGER.atInfo().log("Hello from " + this.getName() + " version " + this.getManifest().getVersion().toString());
+    }
+
+    @Override
+    protected void setup() {
+        LOGGER.atInfo().log("Setting up plugin " + this.getName());
+    }
+}
